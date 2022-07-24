244c24759ece1461652611e6088023cb59460369
luisa malaver <lmalaver@360pmi.com>
2022-07-24 14:35:13 -0500

    add(elements) header

--- components/elements/Header/Header.js
+++ components/elements/Header/Header.js
@@ -1,10 +1,60 @@
+import Image from "next/image"
+import { useState } from "react"
+import { Input } from "../Input/Input"
 import styles from "./Header.module.scss"
 
 export const Header = () => {
 
-  return(
+  const [isActive, setIsActive] = useState(false)
+
+  return (
     <header className={styles.header}>
-      <h1>header</h1>
+      <button
+        id="button__menu"
+        className={`${styles["header__hamburger"]} `}
+        type="button"
+        aria-label="Open menu"
+        onClick={() => setIsActive(!isActive)}
+      >
+        <span className={`${styles["header__hamburger--box"]}`}>
+          <span
+            className={`
+                ${styles["header__hamburger--box_inner"]} 
+                ${isActive && "active"}`}
+          />
+        </span>
+      </button>
+      <div className={`${styles["header__nav"]} ${isActive ? "active" : "disabled"}`}>
+
+      </div>
+      <div className={`${styles["header__logo"]}`}>
+        <div className="desktop">
+          <Image
+            src="/images/logo.svg"
+            alt="logo mejor con salud"
+            width="216px"
+            height="38px"
+          />
+        </div>
+        <div className="mobile">
+          <Image
+            src="/icons/ic-logo.svg"
+            alt="logo mejor con salud"
+            width="60px"
+            height="40px"
+          />
+        </div>
+      </div>
+      <div className={`${styles["header__search"]}`}>
+        <Input
+          addClass="icon ic-search"
+          data={{
+            type: "text",
+            id: "search-header",
+            name: "search-header",
+          }}
+        />
+      </div>
     </header>
-  )
+  );
 }
--- components/elements/index.js
+++ components/elements/index.js
@@ -1,1 +1,3 @@
 export * from "./Header/Header";
+export * from "./Input/Input";
+
