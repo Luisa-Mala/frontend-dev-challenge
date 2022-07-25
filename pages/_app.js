c123089801dffd20d9ecece7459f6d0b7b547bcc
luisa malaver <lmalaver@360pmi.com>
2022-07-25 18:18:02 -0500

    add(package) boostrap-grid

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import "../sass/styles.scss";
+import "bootstrap/dist/css/bootstrap-grid.css";
+import "@/sass/styles.scss";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Component {...pageProps} />;
 }
 
-export default MyApp
+export default MyApp;
