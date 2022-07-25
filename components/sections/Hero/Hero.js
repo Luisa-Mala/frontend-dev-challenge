baee1f72c953e671812ef1540979952059311aee
luisa malaver <lmalaver@360pmi.com>
2022-07-25 17:22:04 -0500

    add(section) hero

--- components/sections/Hero/Hero.js
+++ components/sections/Hero/Hero.js
@@ -1,5 +1,73 @@
+import "swiper/css";
+import "swiper/css/pagination";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper";
+import styles from "./Hero.module.scss";
+import Image from "next/image";
+
 export const Hero = () => {
-  return(
-    <section></section>
-  )
-}
+  return (
+    <section className={`${styles["hero"]}`}>
+      <Swiper
+        pagination={true}
+        modules={[Pagination, Autoplay]}
+        className={`${styles["hero__slider"]}`}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false,
+        }}
+      >
+        <SwiperSlide>
+          <div className="imageNext">
+            <Image
+              src="/images/hero_1.png"
+              alt="hero-image"
+              className="image"
+              layout="fill"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="imageNext">
+            <Image
+              src="/images/hero_2.png"
+              alt="hero-image"
+              className="image"
+              layout="fill"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="imageNext">
+            <Image
+              src="/images/hero_3.png"
+              alt="hero-image"
+              className="image"
+              layout="fill"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="imageNext">
+            <Image
+              src="/images/hero_4.png"
+              alt="hero-image"
+              className="image"
+              layout="fill"
+            />
+          </div>
+        </SwiperSlide>
+        <SwiperSlide>
+          <div className="imageNext">
+            <Image
+              src="/images/hero_5.png"
+              alt="hero-image"
+              className="image"
+              layout="fill"
+            />
+          </div>
+        </SwiperSlide>
+      </Swiper>
+    </section>
+  );
+};
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,10 @@
+import { Hero } from '@/components/sections'
 import { Layout } from '@/components/sections/Layout'
 
 export default function Home() {
   return (
     <Layout title="Home">
-
+      <Hero/>
     </Layout>
     
   )
