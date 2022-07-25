9da2ab09984fed0979fc802266c4f4ed2d8a8e0a
luisa malaver <lmalaver@360pmi.com>
2022-07-25 16:35:25 -0500

    add(header) styles

--- components/elements/Header/Header.js
+++ components/elements/Header/Header.js
@@ -2,6 +2,7 @@ import Image from "next/image"
 import { useState } from "react"
 import { Input } from "../Input/Input"
 import styles from "./Header.module.scss"
+import {data} from "./dataHeader"
 
 export const Header = () => {
 
@@ -25,7 +26,18 @@ export const Header = () => {
         </span>
       </button>
       <div className={`${styles["header__nav"]} ${isActive ? "active" : "disabled"}`}>
-
+        <nav>
+          {data.menu.map((item, index) =>(
+            <div className={`${styles["header__nav--item"]}`} key={"nav"+index}>
+              <p className="category">{item.category}</p>
+              <ul>
+                {item.nav.map((linkItem, idx) => (
+                  <li key={"linkItem"+idx}><a href={linkItem.url}>{linkItem.label}</a></li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </nav>
       </div>
       <div className={`${styles["header__logo"]}`}>
         <div className="desktop">
---
+++ components/elements/Header/dataHeader.js
@@ -0,0 +1,38 @@
+export const data = {
+  menu: [
+    {
+      category: "Embarazo",
+      nav: [
+        {
+          url: "/",
+          label: "Quiero ser mamá",
+        },
+        {
+          url: "/",
+          label: "Voy a ser mamá",
+        },
+        {
+          url: "/",
+          label: "Parto",
+        },
+      ],
+    },
+    {
+      category: "Educación",
+      nav: [
+        {
+          url: "/",
+          label: "Aprende a ser mamá",
+        },
+        {
+          url: "/",
+          label: "Educación para bebés",
+        },
+        {
+          url: "/",
+          label: "Educación para niños",
+        },
+      ],
+    },
+  ],
+};
